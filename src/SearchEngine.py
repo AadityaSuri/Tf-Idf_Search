caceaf6c3d1cd54cd7dd5709611a84e3d649995f
Aaditya Suri <aadityasuri1@gmail.com>
2023-04-03 18:23:55 -0700

    comments and cleaning up

--- src/SearchEngine.py
+++ src/SearchEngine.py
@@ -1,7 +1,6 @@
 
 import pandas as pd
 import string
-# import nltk
 from collections import Counter
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
@@ -18,22 +17,36 @@ import sqlite3
 # nltk.download('stopwords')
 
 
+
+# SearchEngine main class
 class SearchEngine:
+
+    # constructor
     def __init__(self, source_path: 'str') -> None:
-        self.source_path = source_path
+        self.source_path = source_path  # path to the source directory at which all documents are to be searched
         self.database = source_path + "/searchUtils"
 
 
+        # create tfidfMatrix for all terms present in the source directory if it doesn't exist
         if not os.path.exists(self.database):
+
+            # create database directory
             os.mkdir(self.database)
-            self.__doclist = self.__fileCollector()
-            self.__docNmap = {}
+
+            # create tfidfmap.db
+            self.__doclist = self.__fileCollector()  # recursively list all documents in the source directory
+            self.__docNmap = {}                      # map of document name to number of terms in the document
             self.__tfidfMapBuilder()
+
+            # save tfidfmap for searching queries
             conn = sqlite3.connect(self.database + '/tfidfmap.db')
             self.__tfidfMap.to_sql('tfidfmap', conn, if_exists='replace')
             conn.close()
             print("CHECKPOINT: tfidfmap.db created")
+
+        # load tfidfmap for searching queries from existing database
         else:
+            # load tfidfmap
             conn = sqlite3.connect(self.database + '/tfidfmap.db')
             self.__tfidfMap = pd.read_sql_query("SELECT * FROM tfidfmap", conn, index_col='index')
             conn.close()
@@ -46,14 +59,16 @@ class SearchEngine:
 
 
 
-
+    # print top 5 most relevant documents for the given query
     def search(self, query: 'str') -> None:
-        self.__addQuery(query)
+        self.__addQuery(query)  # add query to the tfidfMap
 
-        tfidfMatrix = self.__mapToMatrix()
+        tfidfMatrix = self.__mapToMatrix()  # convert tfidfMap to tfidfMatrix with the query added
         # tfidfMatrix.to_csv('tfidfMatrix.csv')
         # print(tfidfMatrix.index)
 
+
+        # extract the document and query vectors from the matrix
         doc_tfidfMatrix = tfidfMatrix.loc[:, tfidfMatrix.columns != 'query'].to_numpy().round(decimals=4)
         query_vector = tfidfMatrix.loc[:, "query"].to_numpy().round(decimals=4)
 
@@ -66,25 +81,26 @@ class SearchEngine:
 
         scoremap = dict(sorted(scoremap.items(), key=lambda item:item[1]))
 
-
-
         for doc in list(scoremap.items())[:5]:
             print(doc)
 
-        # print(scoremap)
-
     
 
+    # add query to the tfidfMap
     def __addQuery(self, query : 'str') -> None:
+
+        # write query to a file (inefficient, but works. need to change this)
         queryfile = open('query.txt', 'w')
         queryfile.write(query)
         queryfile.close()
 
+        # perform document preprocessing on the query
         tf_res = self.__tf(self.__docPreProcessing('query.txt'))
         query_tf = tf_res
+
+        # add query to the tfidfMap and update the df of each term
         self.__docNmap['query'] = sum(tf_res.values())
         self.__doclist.append('query')
-
         self.__tfidfMap['query'] = 0
 
         for term in query_tf:
@@ -100,21 +116,27 @@ class SearchEngine:
 
 
 
+    # recusively collect all files in the source directory
     def __fileCollector(self) -> 'list':
         filelist = []
         for root, dirs, files in os.walk(self.source_path):
             for file in files:
-                if file.endswith(".json") or file.endswith(".db"):
+                if file.endswith(".json") or file.endswith(".db"):  # ignore json and db files since they are for internal use, need to change this
                     continue
                 else:
                     filelist.append(os.path.join(root, file))
 
-                if len(filelist) == 50:
+                if len(filelist) == 50:  # cap the number of documents to 50 for now, need to change this
                     return filelist
 
         return filelist
     
     
+    # perform some preprocessing on the document
+    # 1. remove any stopwords from the document (using nltk stopwords package) 
+    # 2. remove any punctuation from the document
+    # 3. convert all words to lowercase
+    # 4. perform stemming on the document (using nltk PorterStemmer)
     def __docPreProcessing(self, filepath: 'str') -> 'list':
         stopwords_dict = Counter(stopwords.words('english'))
         ps = PorterStemmer()
@@ -132,6 +154,10 @@ class SearchEngine:
 
 
 
+    # calculate the tf of each term in the document
+    # tf = (number of times term t appears in a document) / (total number of (non unique) terms in the document)
+
+    # kinda slow, maybe optimize this using c++ bindings
     def __tf(self, wordlist : 'list') -> 'dict':
         wordmap = {}
         
@@ -144,7 +170,9 @@ class SearchEngine:
         return wordmap
     
 
-    
+
+
+    # Build the tfidfMap from the doclist 
     def __tfidfMapBuilder(self) -> None:
         termset = set()
 
@@ -153,6 +181,7 @@ class SearchEngine:
             self.__docNmap[doc] = sum(tf_res.values())
             termset.update(set(tf_res.keys()))
 
+        # save docNmap to json file
         with open(self.database + '/docNmap.json', 'w') as f:
             json.dump(self.__docNmap, f, indent=4)
 
@@ -170,10 +199,12 @@ class SearchEngine:
 
 
 
+    # map the tfidfMap to a matrix, remove the df column and normalize the matrix
     def __mapToMatrix(self) -> 'pd.DataFrame':
         matrix = copy.copy(self.__tfidfMap)
 
         N = len(self.__doclist)
+        # df = number of documents that contain the term t (document frequency) 
         matrix['df'] = matrix['df'].apply(lambda x: math.log((N + 1)/(x + 1)) + 1)
 
         for col in matrix.columns:
@@ -184,7 +215,7 @@ class SearchEngine:
             term_df = row['df']
             row = row.apply(lambda x: x * term_df)
         
-        matrix = matrix.drop(columns=['df'], axis=1)
+        matrix = matrix.drop(columns=['df'], axis=1) # remove the df column
 
         return matrix
     
@@ -201,7 +232,7 @@ class SearchEngine:
         divisors = q_mag * D_mags
         cos_thetas = np.divide(qTD, divisors)
         
-        cos_thetas = np.clip(cos_thetas, -1, 1)
+        cos_thetas = np.clip(cos_thetas, -1, 1)  # clip values to [-1, 1] to avoid nan values
         
         scores = np.arccos(cos_thetas)
         
