45d3fac634f5e0f01efe1ac47c0a7e2d63586c50
Aaditya Suri <aadityasuri1@gmail.com>
2023-04-08 01:41:37 -0700

    score values fixed, error with mimicing replace strategy while reading file

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 from SearchEngine import SearchEngine
 import time
-import cppbindings
+# import cppbindings
 
 constructorStartTime = time.process_time()
-engine = SearchEngine(r"../testset/sci.space", max_docs=50)
+engine = SearchEngine(r"../testset/sci.space", max_docs=100)
 print("Time taken to construct: ", time.process_time() - constructorStartTime)
 
 # database = r'../testset/sci.space/tfidfmap.db'
