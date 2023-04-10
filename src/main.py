c384fffe03720ab7684586d3767499ae28228812
Aaditya Suri <aadityasuri1@gmail.com>
2023-04-09 22:42:20 -0700

    tf bindings work

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from SearchEngine import SearchEngine
 import time
 
+start = time.time()
 engine = SearchEngine(r"../testset/sci.space", max_docs=200)
+print("Time taken to initialize SearchEngine: ", time.time() - start)
 
+start = time.time()
 top_n = engine.search("EM radiation in the solar system")
+print("Time taken to search: ", time.time() - start)
 
 print(top_n)
 
